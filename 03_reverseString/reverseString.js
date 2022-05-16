b0b74ae60d921aafa7aaaf5c92f52ed7403ee712
AndTorGuer <atorresguerrero9@gmail.com>
2022-05-16 14:56:08 -0400

    finished exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
-
+const reverseString = function(word) 
+{
+   return word.split('').reverse().join('');
 };
+
+// Step 1. Use the split() method to return a new array
+// Step 2. Use the reverse() method to reverse the new created array
+// Step 3. Use the join() method to join all elements of the array into a string
+// Step 4. Return the reversed string
+
 
 // Do not edit below this line
 module.exports = reverseString;
