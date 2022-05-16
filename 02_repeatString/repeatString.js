c36efcd4b1fb7f042ef492bfe5e21435d94c6ae4
AndTorGuer <atorresguerrero9@gmail.com>
2022-05-15 21:51:15 -0400

    finished exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+                             // 'hey', 3
+const repeatString = function(aString,aNumber) {
+    if (aNumber < 0) // error handling
+    {
+        return 'ERROR';
+    }
+    let newString = ''; // for this to work it has to be initialized
+    for (let i = 0; i < aNumber; i++)
+    {
+       newString += aString;
+    }
+    return newString
 };
 
 // Do not edit below this line
