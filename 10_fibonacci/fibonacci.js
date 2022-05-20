bee2db1e693fce2457569b978f7b7d8675f10e4d
AndTorGuer <atorresguerrero9@gmail.com>
2022-05-20 16:32:46 -0400

    finished exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,28 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+let num1 = 0;
+let num2 = 1;
+
+if (num < 0) 
+{ return "OOPS"; }
+
+if (num === 0)
+{ return 0; }
+
+for(let i = 1; i < num; i++)
+{
+  const nextNum = num2;
+  num2 = num1 + num2;
+  num1 = nextNum;
+}
+return num2;
 
 };
+
+
+
+
+
+
 
 // Do not edit below this line
 module.exports = fibonacci;
