13f2327e24daa754d2dfb3486074c43e6591e168
AndTorGuer <atorresguerrero9@gmail.com>
2022-05-20 14:52:27 -0400

    finished exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+// \W removes all non-alphanumeric characters 
+// g flag for global search  
+var re =  /[\W]/g;
+var lowRegWord = word.toLowerCase().replace(re,''); 
+let reverseArray = lowRegWord.split('').reverse().join('');
+return lowRegWord === reverseArray;
 };
 
 // Do not edit below this line
