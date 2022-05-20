281266fb76a9520e801956ecefe7d27721dd3743
AndTorGuer <atorresguerrero9@gmail.com>
2022-05-19 22:18:14 -0400

    finished exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(num1,num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1,num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
+const sum = function(nums) {
 	
+  return nums.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  return nums.length
+  ? nums.reduce((accumulator, nextItem) => accumulator * nextItem)
+  : 0;
 };
 
-const power = function() {
-	
+const power = function(num1,num2) {
+	return num1 ** num2;
+  // or Math.pow(num1,num2)
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if(num === 0 || num === 1)
+  {
+    return 1;
+  }
+  else 
+  {
+    return num * factorial(num - 1);
+  }
 };
 
 // Do not edit below this line
